e2530817a16fb104cd8b60f8c21b4eb0bee83bd5
LakshitGupta07 <lakshit.gupta072005@gmail.com>
2026-03-13 19:30:28 +0530

    fixed gsap

--- src/types/gsap-trial.d.ts
+++ src/types/gsap-trial.d.ts
@@ -2,3 +2,8 @@ declare module "gsap-trial/SplitText" {
     const SplitText: any;
     export default SplitText;
 }
+
+declare module "gsap-trial/ScrollSmoother" {
+    const ScrollSmoother: any;
+    export default ScrollSmoother;
+}
